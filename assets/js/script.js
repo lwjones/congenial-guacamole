742434f82c45985b391dedd13a8d345e8baed4f0
Lawrence Jones <git@lwjon.es>
2022-01-19 13:07:21 -0800

    Changed Quiz from class to object
    The quiz class was cumbersome to pass events through functions. Opted to modify class to object.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,79 @@
-class Quiz {
-  constructor () {
-    this.frontMatter = {
-      title: "Coding Quiz Challenge",
-      description:
-        "Try to answer the following code-related questions within the time \
-         limit. Keep in mind that incorrect answers will penalize your \
-         score/time by ten seconds!",
-      cta: "Start Quiz"
-    };
-    this.questions = [
-      {
-        question: "Commonly used data types DO NOT include:",
-        choices: [
-          {
-            text: "strings",
-            correct: false
-          },
-          {
-            text: "booleans",
-            correct: false
-          },
-          {
-            text: "alerts",
-            correct: true
-          },
-          {
-            text: "numbers",
-            correct: false
-          }
-        ]
-      }
-    ];
-  }
-
-  loadQuiz = function() {
-    let title = document.getElementById("title");
-    title.textContent = quiz.frontMatter.title;
-
-    let description = document.getElementById("description");
-    description.textContent = quiz.frontMatter.description;
-
-    let cta = document.getElementById("cta");
-    let startBtn = document.createElement("button");
-    startBtn.appendChild(document.createTextNode(quiz.frontMatter.cta));
-    cta.appendChild(startBtn);
-
-    let mainContent = document.getElementById("main");
-    mainContent.classList.add("center");
-
-    startBtn.addEventListener("click", this.startQuiz);
-  }
-
-  startQuiz = function() {
-    document.getElementById("cta").innerHTML = "";
-    let mainContent = document.getElementById("main");
-    mainContent.classList.add("left");
-    mainContent.classList.remove("center");
-
-    let title = document.getElementById("title");
-    title.textContent = quiz.questions[0].question;
-
-    let description = document.getElementById("description");
-    description.textContent = "";
-
-    for (let idx in quiz.questions[0].choices) {
-      let choicePool = document.getElementById("choices");
-      let choiceBtn = document.createElement("button");
-      choiceBtn.classList.add("left");
-      choiceBtn.appendChild(document.createTextNode(quiz.questions[0].choices[idx].text));
-      choicePool.appendChild(choiceBtn);
+const Quiz = {
+  frontMatter: {
+    title: "Coding Quiz Challenge",
+    description:
+      "Try to answer the following code-related questions within the time \
+        limit. Keep in mind that incorrect answers will penalize your \
+        score/time by ten seconds!",
+    cta: "Start Quiz"
+  },
+  questions: [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choices: [
+        {
+          text: "strings",
+          correct: false
+        },
+        {
+          text: "booleans",
+          correct: false
+        },
+        {
+          text: "alerts",
+          correct: true
+        },
+        {
+          text: "numbers",
+          correct: false
+        }
+      ]
     }
+  ]
+};
+
+let loadQuiz = function() {
+  let title = document.getElementById("title");
+  title.textContent = Quiz.frontMatter.title;
+
+  let description = document.getElementById("description");
+  description.textContent = Quiz.frontMatter.description;
+
+  let cta = document.getElementById("cta");
+  let startBtn = document.createElement("button");
+  startBtn.appendChild(document.createTextNode(Quiz.frontMatter.cta));
+  cta.appendChild(startBtn);
+
+  let mainContent = document.getElementById("main");
+  mainContent.classList.add("center");
+
+  startBtn.addEventListener("click", startQuiz);
+};
+
+let startQuiz = function() {
+  // Clear start quiz button
+  document.getElementById("cta").innerHTML = "";
+
+  let mainContent = document.getElementById("main");
+  mainContent.classList.add("left");
+  mainContent.classList.remove("center");
+
+  let title = document.getElementById("title");
+  title.textContent = Quiz.questions[0].question;
+
+  let description = document.getElementById("description");
+  description.textContent = "";
+
+  let choicePool = document.getElementById("choices");
+
+  // create choices for given question
+  for (let idx in Quiz.questions[0].choices) {
+    let choiceBtn = document.createElement("button");
+    choiceBtn.classList.add("left");
+    choiceBtn.appendChild(document.createTextNode(Quiz.questions[0].choices[idx].text));
+    choiceBtn.setAttribute("data-choice-id", idx);
+    choicePool.appendChild(choiceBtn);
   }
 }
 
-let quiz = new Quiz();
-quiz.loadQuiz();
+loadQuiz();
