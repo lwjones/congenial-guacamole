a2eb490602b141bda9431fc73f1a6a46975541e8
Lawrence Jones <git@lwjon.es>
2022-01-19 11:08:02 -0800

    Load first question upon quiz start

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,8 @@ class Quiz {
       description:
         "Try to answer the following code-related questions within the time \
          limit. Keep in mind that incorrect answers will penalize your \
-         score/time by ten seconds!"
+         score/time by ten seconds!",
+      cta: "Start Quiz"
     };
     this.questions = [
       {
@@ -32,17 +33,41 @@ class Quiz {
     ];
   }
 
-  startQuiz = function () {
+  loadQuiz = function() {
     let title = document.getElementById("title");
     title.textContent = quiz.frontMatter.title;
 
     let description = document.getElementById("description");
     description.textContent = quiz.frontMatter.description;
 
+    let answerPool = document.getElementById("choices");
+    let startBtn = document.createElement("button");
+    startBtn.appendChild(document.createTextNode(quiz.frontMatter.cta));
+    answerPool.appendChild(startBtn);
+
     let mainContent = document.getElementById("main");
     mainContent.classList.add("center");
+
+    startBtn.addEventListener("click", startQuiz);
+  }
+}
+
+startQuiz = function() {
+  document.getElementById("choices").innerHTML = "";
+
+  let title = document.getElementById("title");
+  title.textContent = quiz.questions[0].question;
+
+  let description = document.getElementById("description");
+  description.textContent = "";
+
+  for (let idx in quiz.questions[0].choices) {
+    let answerPool = document.getElementById("choices");
+    let choiceBtn = document.createElement("button");
+    choiceBtn.appendChild(document.createTextNode(quiz.questions[0].choices[idx].text));
+    answerPool.appendChild(choiceBtn);
   }
 }
 
 let quiz = new Quiz();
-quiz.startQuiz();
+quiz.loadQuiz();
